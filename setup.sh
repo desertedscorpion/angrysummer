754eb4314412a85310cb9512bf14d38ea6fb3247
Emory Merryman <emory.merryman@gmail.com>
2016-05-05 21:26:46 +0000

    add slave

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 credentials /usr/local/lib/credentials.xml &&
     ssh-key /var/private/id_rsa &&
+    create-slave /usr/local/lib/dockerhost.xml &&
     create-job /usr/local/lib/job.xml &&
     true
