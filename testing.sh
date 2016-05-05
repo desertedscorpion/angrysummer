50cfeb1627f3468d2c8dc2afe8910349c87f359d
Emory Merryman <emory.merryman@gmail.com>
2016-05-05 21:42:53 +0000

    fixing a test

--- testing.sh
+++ testing.sh
@@ -2,7 +2,7 @@
 
 docker build -t ninthgrimmercury/angrysummer . &&
     docker build -t freakygamma/angrysummer test &&
-    if docker run --interactive --tty freakygamma/easternmoose dnf update --assumeyes | grep "^Last metadata expiration check: 0:00:"
+    if docker run --interactive --tty freakygamma/angrysummer dnf update --assumeyes | grep "^Last metadata expiration check: 0:00:"
     then
 	echo dnf was updated within the last hour &&
 	    true
