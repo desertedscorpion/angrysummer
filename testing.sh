d803421859bb9691ebb441756ded135a30466611
Emory Merryman <emory.merryman@gmail.com>
2016-05-05 21:18:00 +0000

    testing that dnf was updated recently.

--- testing.sh
+++ testing.sh
@@ -2,6 +2,15 @@
 
 docker build -t ninthgrimmercury/angrysummer . &&
     docker build -t freakygamma/angrysummer test &&
+    if docker run --interactive --tty freakygamma/easternmoose dnf update --assumeyes | grep "^Last metadata expiration check: 0:00:"
+    then
+	echo dnf was updated within the last hour &&
+	    true
+    else
+	echo dnf was not updated within the last hour &&
+	    exit 64 &&
+	    true
+    fi &&
     docker rm $(docker stop $(docker ps -a -q --filter ancestor=freakygamma/angrysummer --format="{{.ID}}")) &&
     docker rmi --force freakygamma/angrysummer &&
     docker rmi --force ninthgrimmercury/angrysummer &&
