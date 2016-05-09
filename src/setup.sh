f6f76a211e90501e8aaff490bc9785109295f9d2
Emory Merryman <emory.merryman@gmail.com>
2016-05-09 18:41:40 +0000

    install git

--- src/setup.sh
+++ src/setup.sh
@@ -3,5 +3,5 @@
 credentials /usr/local/lib/credentials.xml &&
     ssh-key /var/private/id_rsa &&
     create-slave /usr/local/lib/dockerhost.xml &&
-    create-job /usr/local/lib/job.xml &&
+    install-plugin git &&
     true
