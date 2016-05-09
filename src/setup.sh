2f3892c39be6c33167b8e46e36692c3493752e76
Emory Merryman <emory.merryman@gmail.com>
2016-05-09 19:49:28 +0000

    change order of setup

--- src/setup.sh
+++ src/setup.sh
@@ -4,6 +4,6 @@ credentials /usr/local/lib/credentials.xml &&
     ssh-key /var/private/id_rsa &&
     create-slave /usr/local/lib/dockerhost.xml &&
     dnf install --assumeyes git &&
-    install-plugin git &&
     create-job /usr/local/lib/test-master.xml &&
+    install-plugin git &&
     true
