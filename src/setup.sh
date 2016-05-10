19238e5f8185a1087675429d80eb326c941da3db
Emory Merryman <emory.merryman@gmail.com>
2016-05-10 12:29:47 +0000

    use xslt

--- src/setup.sh
+++ src/setup.sh
@@ -4,6 +4,8 @@ credentials /usr/local/lib/credentials.xml &&
     ssh-key /var/private/id_rsa &&
     create-slave /usr/local/lib/dockerhost.xml &&
     dnf install --assumeyes git &&
-    create-job /usr/local/lib/test-master.xml &&
+    TEMPDIR=$(mktemp -d) &&
+    xsltproc --output ${TEMPDIR}/test-master.xml && /usr/local/lib/test-master.xslt.xml /usr/local/lib/test-master.xml &&
+    create-job ${TEMPDIR}/test-master.xml &&
     install-plugin git &&
     true
