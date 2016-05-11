b97012f7014b5dea77dbea9104c0a687de45390b
Emory Merryman <emory.merryman@gmail.com>
2016-05-11 13:28:59 +0000

    removed cruft that was probably preventing install plugin git

--- src/01-setup.sh
+++ src/01-setup.sh
@@ -4,8 +4,5 @@ credentials /usr/local/lib/credentials.xml &&
     ssh-key /var/private/id_rsa &&
     create-slave /usr/local/lib/dockerhost.xml &&
     dnf install --assumeyes git &&
-    TEMPDIR=$(mktemp -d) &&
-    xsltproc --output ${TEMPDIR}/test-master.xml /usr/local/lib/test-master.xslt.xml /usr/local/lib/test-master.xml &&
-    create-job ${TEMPDIR}/test-master.xml &&
     install-plugin git &&
     true
