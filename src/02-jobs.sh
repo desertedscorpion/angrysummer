c634ab65e80c104e215dbe4ca21696d6137192dd
Emory Merryman <emory.merryman@gmail.com>
2016-05-11 13:16:49 +0000

    add parameter to xsltproc

--- src/02-jobs.sh
+++ src/02-jobs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEMPDIR=$(mktemp -d) &&
-    xsltproc /usr/local/lib/test-master.xslt.xml /usr/local/lib/test-master.xml &&
+    xsltproc --stringparam root ${TEMPDIR} /usr/local/lib/test-master.xslt.xml /usr/local/lib/test-master.xml &&
     for FILE in ${TEMPDIR}/*.xml
     do
 	create-job ${FILE} &&
